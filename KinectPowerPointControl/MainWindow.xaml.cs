241cb0b61361ece393ef8721fa7bce1c5922cff9
Marek Jędrycha <marekjedrycha@hotmail.com>
2016-03-16 15:19:09 +0100

    Upload to GIT

---
+++ KinectPowerPointControl/MainWindow.xaml.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace KinectPowerPointControl
+{
+    public partial class MainWindow : Window
+    {
+        KinectSensor sensor;
+        byte[] colorBytes;
+        Skeleton[] skeletons;
+        bool isCirclesVisible = true;
+        bool isForwardGestureActive = false;
+        bool isBackGestureActive = false;
+        SolidColorBrush activeBrush = new SolidColorBrush(Colors.Green);
+        SolidColorBrush inactiveBrush = new SolidColorBrush(Colors.Red);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+            this.KeyDown += new KeyEventHandler(MainWindow_KeyDown);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            sensor = KinectSensor.KinectSensors.FirstOrDefault();
+
+            if (sensor == null)
+            {
+                MessageBox.Show("Aplikacja wymaga Kinecta.");
+                this.Close();
+            }
+
+            sensor.Start();
+
+            sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            sensor.ColorFrameReady += new EventHandler<ColorImageFrameReadyEventArgs>(sensor_ColorFrameReady);
+
+            sensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+
+            sensor.SkeletonStream.Enable();
+            sensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(sensor_SkeletonFrameReady);
+
+           sensor.ElevationAngle = 0;
+
+            Application.Current.Exit += new ExitEventHandler(Current_Exit);
+
+
+        }
+        private void bSetTilt_Click(object sender, RoutedEventArgs e)
+        {
+            if (sensor.IsRunning && sensor != null)
+            {
+                sensor.ElevationAngle = (int)sSetTilt.Value;
+                lTiltValue.Content = sensor.ElevationAngle.ToString();
+            }
+        }
+
+        void Current_Exit(object sender, ExitEventArgs e)
+        {
+            if (sensor != null)
+            {
+                sensor.AudioSource.Stop();
+                sensor.Stop();
+                sensor.Dispose();
+                sensor = null;
+            }
+        }
+
+        void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.C)
+            {
+                ToggleCircles();
+            }
+        }
+
+        void sensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (var image = e.OpenColorImageFrame())
+            {
+                if (image == null)
+                    return;
+
+                if (colorBytes == null ||
+                    colorBytes.Length != image.PixelDataLength)
+                {
+                    colorBytes = new byte[image.PixelDataLength];
+                }
+
+                image.CopyPixelDataTo(colorBytes);
+
+                int length = colorBytes.Length;
+                for (int i = 0; i < length; i += 4)
+                {
+                    colorBytes[i + 3] = 255;
+                }
+
+                BitmapSource source = BitmapSource.Create(image.Width,
+                    image.Height,
+                    96,
+                    96,
+                    PixelFormats.Bgra32,
+                    null,
+                    colorBytes,
+                    image.Width * image.BytesPerPixel);
+                videoImage.Source = source;
+            }
+        }
+        
+        void sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (var skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame == null)
+                    return;
+
+                if (skeletons == null ||
+                    skeletons.Length != skeletonFrame.SkeletonArrayLength)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                }
+
+                skeletonFrame.CopySkeletonDataTo(skeletons);
+            }
+
+            Skeleton closestSkeleton = skeletons.Where(s => s.TrackingState == SkeletonTrackingState.Tracked)
+                                                .OrderBy(s => s.Position.Z * Math.Abs(s.Position.X))
+                                                .FirstOrDefault();
+
+            if (closestSkeleton == null)
+                return;
+
+            var head = closestSkeleton.Joints[JointType.Head];
+            var rightHand = closestSkeleton.Joints[JointType.HandRight];
+            var leftHand = closestSkeleton.Joints[JointType.HandLeft];
+
+            if (head.TrackingState == JointTrackingState.NotTracked ||
+                rightHand.TrackingState == JointTrackingState.NotTracked ||
+                leftHand.TrackingState == JointTrackingState.NotTracked)
+            {
+                return;
+            }
+
+            SetEllipsePosition(ellipseHead, head, false);
+            SetEllipsePosition(ellipseLeftHand, leftHand, isBackGestureActive);
+            SetEllipsePosition(ellipseRightHand, rightHand, isForwardGestureActive);
+
+            ProcessForwardBackGesture(head, rightHand, leftHand);
+        }
+        
+        private void SetEllipsePosition(Ellipse ellipse, Joint joint, bool isHighlighted)
+        {
+            if (isHighlighted)
+            {
+                ellipse.Width = 60;
+                ellipse.Height = 60;
+                ellipse.Fill = activeBrush;
+            }
+            else
+            {
+                ellipse.Width = 20;
+                ellipse.Height = 20;
+                ellipse.Fill = inactiveBrush;
+            }
+
+            CoordinateMapper mapper = sensor.CoordinateMapper;
+
+            var point = mapper.MapSkeletonPointToColorPoint(joint.Position, sensor.ColorStream.Format);
+
+            Canvas.SetLeft(ellipse, point.X - ellipse.ActualWidth / 2);
+            Canvas.SetTop(ellipse, point.Y - ellipse.ActualHeight / 2);
+        }
+        
+        private void ProcessForwardBackGesture(Joint head, Joint rightHand, Joint leftHand)
+        {
+            if (rightHand.Position.X > head.Position.X + 0.45)
+            {
+                if (!isForwardGestureActive)
+                {
+                    isForwardGestureActive = true;
+                    System.Windows.Forms.SendKeys.SendWait("{Right}");
+                }
+            }
+            else
+            {
+                isForwardGestureActive = false;
+            }
+
+            if (leftHand.Position.X < head.Position.X - 0.45)
+            {
+                if (!isBackGestureActive)
+                {
+                    isBackGestureActive = true;
+                    System.Windows.Forms.SendKeys.SendWait("{Left}");
+                }
+            }
+            else
+            {
+                isBackGestureActive = false;
+            }
+        }
+        
+        void ToggleCircles()
+        {
+            if (isCirclesVisible)
+                HideCircles();
+            else
+                ShowCircles();
+        }
+
+        void HideCircles()
+        {
+            isCirclesVisible = false;
+            ellipseHead.Visibility = System.Windows.Visibility.Collapsed;
+            ellipseLeftHand.Visibility = System.Windows.Visibility.Collapsed;
+            ellipseRightHand.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        void ShowCircles()
+        {
+            isCirclesVisible = true;
+            ellipseHead.Visibility = System.Windows.Visibility.Visible;
+            ellipseLeftHand.Visibility = System.Windows.Visibility.Visible;
+            ellipseRightHand.Visibility = System.Windows.Visibility.Visible;
+        }
+    }
+}
